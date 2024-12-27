6c7accf5788129ee248231b93486ca7142223623
AkiraYago <the.programmer.yago@gmail.com>
2024-12-27 18:17:04 -0300

    fix: navbar responsive and useless navitem

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -3,11 +3,10 @@ import Link from "next/link"
 const Header = () => {
   return (
     <header className="text-white p-header-phone md:p-header-tablet lg:p-header-desktop flex justify-center">
-      <nav className="flex justify-center items-center gap-8 w-full max-w-maximum h-[8svh]">
+      <nav className="flex flex-col h-fit sm:flex-row sm:justify-center items-center gap-8 w-full max-w-maximum sm:h-[8svh]">
         <Link className="hover:border-b-4 text-buttons w-[200px] text-center h-fit p-buttons" href="#about">About</Link>
         <Link className="hover:border-b-4 text-buttons w-[200px] text-center h-fit p-buttons" href="#experience">Experience</Link>
         <Link className="hover:border-b-4 text-buttons w-[200px] text-center h-fit p-buttons" href="#projects">Projects</Link>
-        <Link className="hover:border-b-4 text-buttons w-[200px] text-center h-fit p-buttons" href="#contact">Contact</Link>
       </nav>
     </header>
   )
